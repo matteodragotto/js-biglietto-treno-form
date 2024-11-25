682fd18eba1d7d8b3efb66c3c8561aaa53007eb6
matteodragotto <matteodrag8@gmail.com>
2024-11-25 16:00:22 +0100

    [feat] html spans and js submit reader

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 const submit = document.querySelector('.sbm-btn')
+const form = document.querySelector('.form')
+
+const nameField = document.getElementById('name-surname-field')
+const kmField = document.getElementById('km-field')
+const ageField = document.getElementById('age-field')
+
+const name = document.getElementById('name')
+const offer = document.getElementById('offer')
+const carriage = document.getElementById('carriage')
+const cpCode = document.getElementById('cp-code')
+const price = document.getElementById('price')
+
+
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  name.innerText = nameField.value.trim()
+ 
+})
+
+
+
+
