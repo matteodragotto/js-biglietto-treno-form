630a5f22f6cccf4fda3f8c0562647aea1b27c9d0
matteodragotto <matteodrag8@gmail.com>
2024-11-25 16:28:00 +0100

    [feat] javascript full program

--- js/script.js
+++ js/script.js
@@ -12,11 +12,33 @@ const cpCode = document.getElementById('cp-code')
 const price = document.getElementById('price')
 
 
+const kmDaPercorrere = kmField.value.trim();
+
+
+let prezzoPerKm = 0.21;
+const prezzoTotale = prezzoPerKm * kmDaPercorrere
+const scontoUnder18 = 20
+const scontoOver65 = 40
 
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   name.innerText = nameField.value.trim()
+  
+  if (ageField.value === 'minor') {
+    offer.innerText = 'Biglietto Minori';
+    price.innerText = prezzoTotale - (prezzoTotale * scontoUnder18 / 100).toFixed(2) + '€'
+  } else if (ageField.value === 'senior') {
+    offer.innerText = 'Biglietto Senior';
+    price.innerText = prezzoTotale - (prezzoTotale * scontoOver65 / 100).toFixed(2) + '€'
+  } else {
+    offer.innerText = 'Biglietto Standard';
+    price.innerText = prezzoTotale.toFixed(2) + '€'
+  }
+
+  carriage.innerText = Math.ceil(Math.random() * 20)
+
+  cpCode.innerText = Math.ceil(Math.random()* 100000)
  
 })
 
